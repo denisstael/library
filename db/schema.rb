8969e62bcdd2f96fab5e667876a578ca368ed874
Denis Stael <denis@nullbug.dev>
2023-11-04 05:53:24 -0300

    Updated project

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_030114) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_05_11_030114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,8 +25,8 @@ ActiveRecord::Schema.define(version: 2021_05_11_030114) do
     t.integer "pages_number"
     t.integer "edition_year"
     t.integer "exemplary"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "loans", force: :cascade do |t|
@@ -35,8 +34,8 @@ ActiveRecord::Schema.define(version: 2021_05_11_030114) do
     t.date "devolution_date", null: false
     t.date "discharge_date"
     t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "book_id"
     t.bigint "reader_id"
     t.index ["book_id"], name: "index_loans_on_book_id"
@@ -48,16 +47,16 @@ ActiveRecord::Schema.define(version: 2021_05_11_030114) do
     t.string "phone"
     t.string "email"
     t.date "birth_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "loans", "books"
